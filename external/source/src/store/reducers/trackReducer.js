61a3aaf33956256cec753fa245b172cea4a58f28
bingex <romandatsyuk@gmail.com>
2017-06-14 00:39:01 +0300

    Add filters component without filter logic.

--- external/source/src/store/reducers/trackReducer.js
+++ external/source/src/store/reducers/trackReducer.js
@@ -1,11 +1,15 @@
 import {
   SET_TRACKS,
   TOGGLE_SINGLE_TRACK,
-  TOGGLE_TRACK_FILTERS
+  TOGGLE_TRACK_FILTERS,
+  SET_WORK_TYPES,
+  SET_STATUS_TYPES
 } from './../actions/types';
 
 const initial = {
   tracks: [],
+  workTypes: [],
+  statusTypes: [],
   trackIsOpen: false,
   filtersIsOpen: false
 };
@@ -14,6 +18,10 @@ export default function trackReducer(state = initial, action = {}) {
   switch (action.type) {
     case SET_TRACKS:
       return Object.assign({}, state, { tracks: action.tracks });
+    case SET_STATUS_TYPES:
+      return Object.assign({}, state, { statusTypes: action.statusTypes });
+    case SET_WORK_TYPES:
+      return Object.assign({}, state, { workTypes: action.workTypes });
     case TOGGLE_SINGLE_TRACK:
       return Object.assign({}, state, { trackIsOpen: !state.trackIsOpen });
     case TOGGLE_TRACK_FILTERS:
