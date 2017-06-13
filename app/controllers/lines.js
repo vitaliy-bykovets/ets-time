75f0468772cf367debf2d77ce95002590e2bec6b
mazahell <ipmazahell@gmail.com>
2017-06-13 10:57:21 +0300

    List tracklines

--- app/controllers/lines.js
+++ app/controllers/lines.js
@@ -4,16 +4,76 @@ const mw = require('./../middlewares/index');
 const Line = require('./../models/Line');
 const knex = require('./../libs/Db').knex;
 const _ = require('lodash');
+const async = require('async');
+
+const criteriaForList = function(param) {
+  return function() {
+    if (param.user_name) {
+      this.where('u.first_name', 'like', '%' + param.user_name + '%').orWhere(
+        'u.last_name',
+        'like',
+        '%' + param.user_name + '%'
+      );
+    }
+    if (param.project) {
+      this.where('tl.project', 'like', '%' + param.project + '%');
+    }
+    if (param.date_start) {
+      this.where('tl.date_task', '>=', param.date_start);
+    }
+    if (param.date_end) {
+      this.where('tl.date_task', '<=', param.date_end);
+    }
+    if (param.status) {
+      this.where('tl.status', param.status);
+    }
+    if (param.type_work) {
+      this.where('tl.type_work', param.type_work);
+    }
+  };
+};
 
 /* GET users listing. */
-router.get('/', mw.validators.line_list, (req, res) => {
-  knex('track_lines')
-    .then(data => {
-      console.log('data', data);
-    })
-    .catch(e => {
-      console.log('e', e);
-    });
+router.get('/', mw.validators.line_list, async (req, res) => {
+  let param = req.query;
+
+  async.parallel(
+    {
+      count: callback => {
+        knex('track_lines as tl')
+          .where(criteriaForList(param))
+          .leftJoin('users as u', 'u.id', 'tl.user_id')
+          .first()
+          .count('* as c')
+          .asCallback(callback);
+      },
+      list: callback => {
+        knex('track_lines as tl')
+          .select('tl.*', 'u.first_name', 'u.last_name', 'u.user_type', 'u.locked', 'u.email')
+          .where(criteriaForList(param))
+          .leftJoin('users as u', 'u.id', 'tl.user_id')
+          .orderBy('tl.created_at', 'desc')
+          .asCallback(callback);
+      }
+    },
+    (err, results) => {
+      if (err) {
+        res.status(400).end();
+      } else if (results) {
+        res.json({
+          count: results.count.c,
+          data: results.list
+        });
+      }
+    }
+  );
+
+  //let count = ;
+  //res.json({ count: count.c });
+
+  //knex('track_lines').then(data => {}).catch(e => {
+  //  console.log('e', e);
+  //});
 });
 
 router.post('/', mw.validators.line_create, (req, res) => {
--- app/middlewares/validators/line_list.js
+++ app/middlewares/validators/line_list.js
@@ -5,8 +5,8 @@ module.exports = (req, res, next) => {
   const rules = {
     user_name: 'string|min:3',
     project: 'string|min:2',
-    date_start: '|regex:/^\\d{4}-\\d{2}-\\d{2}$/|date',
-    date_end: '|regex:/^\\d{4}-\\d{2}-\\d{2}$/|date',
+    date_start: 'regex:/^\\d{4}-\\d{2}-\\d{2}$/|date',
+    date_end: 'regex:/^\\d{4}-\\d{2}-\\d{2}$/|date',
     status: 'string|in:' + env.task_status.join(','),
     type_work: 'string|in:' + env.type_works.join(',')
   };
