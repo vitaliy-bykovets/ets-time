517596c6daf942d6f8e970ada373d3737d89221b
solomko2 <solomko2@yandex.ru>
2017-07-12 17:11:38 +0300

    hide in members nav

--- external/source/src/components/general/Topbar.js
+++ external/source/src/components/general/Topbar.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import classnames from 'classnames';
-import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
+import React from "react";
+import classnames from "classnames";
+import { connect } from "react-redux";
+import { Link, withRouter } from "react-router-dom";
 
 // Components
-import Settings from './Settings';
+import Settings from "./Settings";
 
 // Icons
-import FaBlocks from 'react-icons/lib/fa/th';
-import FaLines from 'react-icons/lib/fa/align-justify';
-import FaCog from 'react-icons/lib/fa/cog';
-import FaPower from 'react-icons/lib/fa/power-off';
-import logo from './../../data/selecto.svg';
-import TrackIcon from 'react-icons/lib/ti/time';
-import UserIcon from 'react-icons/lib/fa/user';
-import GeneralSkills from 'react-icons/lib/go/repo';
-import UserSkills from 'react-icons/lib/fa/graduation-cap';
-import ChartIcon from 'react-icons/lib/fa/pie-chart';
+import FaBlocks from "react-icons/lib/fa/th";
+import FaLines from "react-icons/lib/fa/align-justify";
+import FaCog from "react-icons/lib/fa/cog";
+import FaPower from "react-icons/lib/fa/power-off";
+import logo from "./../../data/selecto.svg";
+import TrackIcon from "react-icons/lib/ti/time";
+import UserIcon from "react-icons/lib/fa/user";
+import GeneralSkills from "react-icons/lib/go/repo";
+import UserSkills from "react-icons/lib/fa/graduation-cap";
+import ChartIcon from "react-icons/lib/fa/pie-chart";
 
 // Actions
-import { changeTrackView } from './../../store/actions/trackActions';
-import { setActiveUser } from './../../store/actions/userActions';
-import { setToken } from './../../store/actions/generalActions';
+import { changeTrackView } from "./../../store/actions/trackActions";
+import { setActiveUser } from "./../../store/actions/userActions";
+import { setToken } from "./../../store/actions/generalActions";
 
 // Helpers
-import { getFirstLetter } from './../../shared/HelpService';
+import { getFirstLetter } from "./../../shared/HelpService";
 
 // Actions
-import { getLibraries } from './../../store/actions/trackActions';
+import { getLibraries } from "./../../store/actions/trackActions";
 
 class Topbar extends React.Component {
   state = {
@@ -41,7 +41,7 @@ class Topbar extends React.Component {
 
   changeView = view => {
     this.props.changeTrackView(view);
-    localStorage.setItem('viewType', view);
+    localStorage.setItem("viewType", view);
   };
 
   toggleSettings = () => {
@@ -49,32 +49,38 @@ class Topbar extends React.Component {
   };
 
   logoutHandler = () => {
-    this.props.setActiveUser({ first_name: '', last_name: '' });
-    this.props.setToken('');
-    localStorage.removeItem('token');
-    this.props.history.push('/login');
+    this.props.setActiveUser({ first_name: "", last_name: "" });
+    this.props.setToken("");
+    localStorage.removeItem("token");
+    this.props.history.push("/login");
   };
 
   render() {
     const { viewType, location, bgColor, activeUser } = this.props;
     const { settingsOpen } = this.state;
-    const trackUrl = location.pathname.includes('tracks') || location.pathname === '/';
-    const userUrl = location.pathname.includes('users');
-    const skillsUrl = location.pathname === '/skills';
-    const userSkillsUrl = location.pathname === '/user-skills';
-    const statUrl = location.pathname.includes('stats');
+    const trackUrl = location.pathname.includes("tracks") ||
+      location.pathname === "/";
+    const userUrl = location.pathname.includes("users");
+    const skillsUrl = location.pathname === "/skills";
+    const userSkillsUrl = location.pathname === "/user-skills";
+    const statUrl = location.pathname.includes("stats");
 
-    const isOwnerOrPm =
-      (trackUrl || userUrl || skillsUrl || userSkillsUrl || statUrl) &&
+    const isOwnerOrPm = (trackUrl ||
+      userUrl ||
+      skillsUrl ||
+      userSkillsUrl ||
+      statUrl) &&
       activeUser.roles &&
-      (activeUser.roles.includes('owner') || activeUser.roles.includes('pm'));
+      (activeUser.roles.includes("owner") || activeUser.roles.includes("pm"));
 
     return (
       <div className="topbar">
         <h4 className="topbar__headline">
           <img src={logo} alt="Selecto" height="20px" />
           <span className="p-l-20">
-            {`${getFirstLetter(activeUser.first_name)}. ${activeUser.last_name}`}
+            {
+              `${getFirstLetter(activeUser.first_name)}. ${activeUser.last_name}`
+            }
           </span>
           <FaPower className="logout" onClick={this.logoutHandler} />
         </h4>
@@ -82,19 +88,19 @@ class Topbar extends React.Component {
         <div className="topbar__menu" style={{ color: bgColor }}>
           {trackUrl
             ? <FaBlocks
-                className={classnames('topbar__icon', {
-                  'topbar__icon--active': viewType === 'block'
+                className={classnames("topbar__icon", {
+                  "topbar__icon--active": viewType === "block"
                 })}
-                onClick={() => this.changeView('block')}
+                onClick={() => this.changeView("block")}
               />
             : null}
 
           {trackUrl
             ? <FaLines
-                className={classnames('topbar__icon', 'p-r-20', {
-                  'topbar__icon--active': viewType === 'line'
+                className={classnames("topbar__icon", "p-r-20", {
+                  "topbar__icon--active": viewType === "line"
                 })}
-                onClick={() => this.changeView('line')}
+                onClick={() => this.changeView("line")}
               />
             : null}
 
@@ -102,8 +108,8 @@ class Topbar extends React.Component {
             ? <div className="topbar__menu--wrapper">
                 <Link
                   to="/tracks"
-                  className={classnames('topbar__menu--btn', {
-                    'topbar__menu--active': trackUrl
+                  className={classnames("topbar__menu--btn", {
+                    "topbar__menu--active": trackUrl
                   })}
                   style={{ background: bgColor }}
                 >
@@ -111,8 +117,8 @@ class Topbar extends React.Component {
                 </Link>
                 <Link
                   to="/users"
-                  className={classnames('topbar__menu--btn', {
-                    'topbar__menu--active': userUrl
+                  className={classnames("topbar__menu--btn", {
+                    "topbar__menu--active": userUrl
                   })}
                   style={{ background: bgColor }}
                 >
@@ -120,8 +126,8 @@ class Topbar extends React.Component {
                 </Link>
                 <Link
                   to="/skills"
-                  className={classnames('topbar__menu--btn', {
-                    'topbar__menu--active': skillsUrl
+                  className={classnames("topbar__menu--btn", {
+                    "topbar__menu--active": skillsUrl
                   })}
                   style={{ background: bgColor }}
                 >
@@ -129,8 +135,8 @@ class Topbar extends React.Component {
                 </Link>
                 <Link
                   to="/user-skills"
-                  className={classnames('topbar__menu--btn', {
-                    'topbar__menu--active': userSkillsUrl
+                  className={classnames("topbar__menu--btn", {
+                    "topbar__menu--active": userSkillsUrl
                   })}
                   style={{ background: bgColor }}
                 >
@@ -138,19 +144,35 @@ class Topbar extends React.Component {
                 </Link>
                 <Link
                   to="/stats"
-                  className={classnames('topbar__menu--btn', {
-                    'topbar__menu--active': statUrl
+                  className={classnames("topbar__menu--btn", {
+                    "topbar__menu--active": statUrl
                   })}
                   style={{ background: bgColor }}
                 >
                   <ChartIcon />
                 </Link>
               </div>
-            : null}
+            : <div className="topbar__menu--wrapper">
+                <Link
+                  to="/tracks"
+                  className={classnames("topbar__menu--btn", {
+                    "topbar__menu--active": trackUrl
+                  })}
+                  style={{ background: bgColor }}
+                >
+                  <TrackIcon />
+                </Link>
+              </div>}
 
           <div className="topbar__icons" style={{ color: bgColor }}>
-            <FaCog className="topbar__icon p-l-20" onClick={this.toggleSettings} />
-            <Settings settingsOpen={settingsOpen} toggleSettings={this.toggleSettings} />
+            <FaCog
+              className="topbar__icon p-l-20"
+              onClick={this.toggleSettings}
+            />
+            <Settings
+              settingsOpen={settingsOpen}
+              toggleSettings={this.toggleSettings}
+            />
           </div>
         </div>
       </div>
