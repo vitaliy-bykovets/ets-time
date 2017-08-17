26a4dec4def07dee9574d2f348dcf41655dad955
mazahell <ipmazahell@gmail.com>
2017-08-17 18:18:16 +0300

    Ad sorting users by status, first_name

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -42,6 +42,7 @@ router.get('/', users_list, async (req, res) => {
         knex('users')
           .select('first_name', 'last_name', 'roles', 'position', 'email', 'rate', 'id', 'locked')
           .where(criteriaForList(param))
+          .orderBy('locked', 'asc')
           .orderBy('first_name', 'asc')
           .asCallback(callback);
       }
