80e45833d40bc73672c10e4eebf9df0826036175
mazahell <ipmazahell@gmail.com>
2017-08-17 11:33:13 +0300

    Add English skill

--- migrations/20170817104454_AddEnglishSkill.js
+++ migrations/20170817104454_AddEnglishSkill.js
@@ -2,27 +2,7 @@ exports.up = knex => {
   return knex.table('skills').insert([
     {
       parent_id: null,
-      name: 'NodeJS',
-      desc: null
-    },
-    {
-      parent_id: null,
-      name: 'Python',
-      desc: null
-    },
-    {
-      parent_id: null,
-      name: 'Android',
-      desc: null
-    },
-    {
-      parent_id: null,
-      name: 'Databases',
-      desc: null
-    },
-    {
-      parent_id: null,
-      name: 'QA',
+      name: 'English',
       desc: null
     }
   ]);
